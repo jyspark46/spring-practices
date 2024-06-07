95857f090a3b975be99c066146661c6ef40b3541
jelypvid <jyspark46@gmail.com>
2024-06-07 13:24:49 +0900

    guestbook3 controller update - transaction practices

--- guestbook3/src/main/java/com/poscodx/guestbook/controller/GuestbookController.java
+++ guestbook3/src/main/java/com/poscodx/guestbook/controller/GuestbookController.java
@@ -10,25 +10,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.poscodx.guestbook.repository.GuestbookRepositoryWithJdbcContext;
-import com.poscodx.guestbook.repository.GuestbookRepositoryWithJdbcTemplate;
-import com.poscodx.guestbook.repository.GuestbookRepositoryWithRawJdbc;
+import com.poscodx.guestbook.repository.GuestbookRepository;
 import com.poscodx.guestbook.vo.GuestbookVo;
 
 @Controller
 public class GuestbookController {
-	@Autowired
-	private GuestbookRepositoryWithRawJdbc guestbookRepository1;
+	/*@Autowired
+	private GuestbookRepositoryWithRawJdbc guestbookRepository1;*/ // guestbook2로 이동 !!
 	
 	@Autowired
-	private GuestbookRepositoryWithJdbcContext guestbookRepository2; // find() 는 안됨..
+	private GuestbookRepository guestbookRepository;
 	
-	@Autowired
-	private GuestbookRepositoryWithJdbcTemplate guestbookRepository3;
+	/*@Autowired
+	private GuestbookRepositoryWithJdbcTemplate guestbookRepository3;*/ // 
 	
 	@RequestMapping("/")
 	public String index(Model model) {
-		List<GuestbookVo> list = guestbookRepository3.findAll();
+		List<GuestbookVo> list = guestbookRepository.findAll();
 		model.addAttribute("list", list);
 		
 		return "index";
@@ -36,7 +34,7 @@ public class GuestbookController {
 
 	@RequestMapping("/add")
 	public String add(GuestbookVo vo) {
-		guestbookRepository3.insert(vo);
+		guestbookRepository.insert(vo);
 		
 		return "redirect:/";
 	}
@@ -50,7 +48,7 @@ public class GuestbookController {
 	
 	@RequestMapping(value="/delete/{no}", method=RequestMethod.POST)
 	public String delete(@PathVariable("no") Long no, @RequestParam(value="password", required=true, defaultValue="") String password) {
-		guestbookRepository3.deleteByNoAndPassword(no, password);
+		guestbookRepository.deleteByNoAndPassword(no, password);
 		
 		return "redirect:/";
 	}
