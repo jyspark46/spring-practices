ce2118003721f0e38ce9428f35d3ce7b5ed1ce7d
jelypvid <jyspark46@gmail.com>
2024-06-18 10:04:55 +0900

    Explicit Configuration - XML

--- container/src/test/java/com/poscodx/container/videosystem/DVDPlayerXmlConfigTest.java
+++ container/src/test/java/com/poscodx/container/videosystem/DVDPlayerXmlConfigTest.java
@@ -35,6 +35,29 @@ public class DVDPlayerXmlConfigTest {
 	@Qualifier("avengersAgeOfUltron")
 	DigitalVideoDisc dvd5;
 	
+	@Autowired
+	@Qualifier("avengersCaptainAmerica")
+	DigitalVideoDisc dvd6;
+	
+	@Autowired
+	@Qualifier("avengersDirectorEdition")
+	DigitalVideoDisc dvd7;
+	
+	@Autowired
+	DVDPack dvdPack;
+	
+	@Autowired
+	@Qualifier("avengersExpansionEdition1")
+	DigitalVideoDisc dvd8;
+	
+	@Autowired
+	@Qualifier("avengersExpansionEdition2")
+	DigitalVideoDisc dvd9;
+	
+	@Autowired
+	@Qualifier("avengersExpansionEdition3")
+	DigitalVideoDisc dvd10;
+	
 	@Disabled // test 실행 X
 	@Test
 	public void testDVD1() {
@@ -59,5 +82,35 @@ public class DVDPlayerXmlConfigTest {
 	@Test
 	public void testDVD5() {
 		assertNotNull(dvd5);
+	}
+	
+	@Test
+	public void testDVD6() {
+		assertNotNull(dvd6);
+	}
+	
+	@Test
+	public void testDVD7() {
+		assertNotNull(dvd7);
+	}
+	
+	@Test
+	public void testDVDPack() {
+		assertNotNull(dvdPack);
+	}
+	
+	@Test
+	public void testDVD8() {
+		assertNotNull(dvd8);
+	}
+	
+	@Test
+	public void testDVD9() {
+		assertNotNull(dvd9);
+	}
+	
+	@Test
+	public void testDVD10() {
+		assertNotNull(dvd10);
 	}
 }
